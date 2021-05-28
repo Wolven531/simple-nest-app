761b015c438477635b6efbb2dc0be3478223c30a
wolven531 <anthony.williams.cs@gmail.com>
2021-05-28 15:37:58 -0600

    Add summoner svc

---
+++ src/services/summoner.service.spec.ts
@@ -0,0 +1,143 @@
+import { Summoner } from '../models/summoner.model'
+import { HttpModule, HttpService, HttpStatus, Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Test, TestingModule } from '@nestjs/testing'
+import { AxiosResponse } from 'axios'
+import { from } from 'rxjs'
+import { toggleMockedLogger } from '../../test/utils'
+import { SummonerService } from './summoner.service'
+
+type TestCase_SearchByName = {
+	descriptionMockedBehavior: string
+	expectedCountGet: number
+	expectedResult: Summoner | null
+	mockHttpGet: jest.Mock
+	param: string
+}
+
+describe('Summoner Service', () => {
+	const fakeAPIKey = 'some-api-key'
+	const testCases_searchByName: TestCase_SearchByName[] = [
+		{
+			descriptionMockedBehavior: 'request fails',
+			expectedCountGet: 1,
+			expectedResult: null,
+			mockHttpGet: jest.fn(() =>
+				from(Promise.reject(new Error('fake AJW error'))),
+			),
+			param: '',
+		},
+		{
+			descriptionMockedBehavior: 'request succeeds w/ NOT_FOUND',
+			expectedCountGet: 1,
+			expectedResult: null,
+			mockHttpGet: jest.fn(() =>
+				from(
+					Promise.resolve({
+						status: HttpStatus.NOT_FOUND,
+					} as AxiosResponse<Summoner>),
+				),
+			),
+			param: '',
+		},
+		{
+			descriptionMockedBehavior: 'request succeeds and has data',
+			expectedCountGet: 1,
+			expectedResult: { name: 'summ-name-1' } as Summoner,
+			mockHttpGet: jest.fn(() =>
+				from(
+					Promise.resolve({
+						data: { name: 'summ-name-1' } as Summoner,
+						status: HttpStatus.OK,
+					} as AxiosResponse<Summoner>),
+				),
+			),
+			param: 'summ-name-1',
+		},
+	]
+	let service: SummonerService
+	let testModule: TestingModule
+
+	beforeEach(async () => {
+		testModule = await Test.createTestingModule({
+			controllers: [],
+			imports: [HttpModule],
+			providers: [
+				{
+					provide: ConfigService,
+					useFactory: () => ({
+						get: jest.fn().mockReturnValue(fakeAPIKey),
+					}),
+				},
+				SummonerService,
+				Logger,
+			],
+		}).compile()
+
+		service = testModule.get(SummonerService)
+	})
+
+	afterEach(async () => {
+		await testModule.close()
+	})
+
+	describe('w/ mocked logger functions [ debug, error, log, verbose ]', () => {
+		beforeEach(() => {
+			toggleMockedLogger(testModule)
+		})
+
+		afterEach(() => {
+			toggleMockedLogger(testModule, false)
+		})
+
+		testCases_searchByName.forEach(
+			({
+				descriptionMockedBehavior,
+				expectedCountGet,
+				expectedResult,
+				mockHttpGet,
+				param,
+			}) => {
+				describe(`w/ mocked HTTP Get (${descriptionMockedBehavior})`, () => {
+					beforeEach(() => {
+						jest
+							.spyOn(testModule.get(HttpService), 'get')
+							.mockImplementation(mockHttpGet)
+					})
+
+					afterEach(() => {
+						jest.spyOn(testModule.get(HttpService), 'get').mockRestore()
+					})
+
+					describe('invoke searchByName()', () => {
+						let actualResult: Summoner | null
+
+						beforeEach(async () => {
+							actualResult = await service.searchByName(param)
+						})
+
+						it('invokes get() correctly and returns expected result', () => {
+							expect(mockHttpGet).toHaveBeenCalledTimes(expectedCountGet)
+
+							if (expectedCountGet > 0) {
+								expect(mockHttpGet).toHaveBeenLastCalledWith(
+									`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${param}`,
+									{
+										headers: {
+											'Accept-Charset':
+												'application/x-www-form-urlencoded; charset=UTF-8',
+											'Accept-Language': 'en-US,en;q=0.9',
+											'X-Riot-Token': fakeAPIKey,
+										},
+									},
+								)
+							}
+
+							expect(actualResult).toEqual(expectedResult)
+						})
+					})
+				})
+			},
+		)
+	})
+})
---
+++ src/services/summoner.service.ts
@@ -0,0 +1,71 @@
+import { Summoner } from '../models/summoner.model'
+import {
+	HttpService,
+	HttpStatus,
+	Inject,
+	Injectable,
+	Logger,
+} from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { AxiosResponse } from 'axios'
+import { ENV_API_KEY, ENV_API_KEY_DEFAULT, REGION } from '../constants'
+
+@Injectable()
+export class SummonerService {
+	private static readonly BASE = `https://${REGION}.api.riotgames.com`
+	private static readonly ENDPOINT_SEARCH_BY_NAME =
+		'lol/summoner/v4/summoners/by-name'
+
+	constructor(
+		@Inject(ConfigService)
+		private readonly configService: ConfigService,
+		@Inject(HttpService)
+		private readonly httpService: HttpService,
+		@Inject(Logger)
+		private readonly logger: Logger,
+	) {}
+
+	/**
+	 * This method uses the Riot API to search for a Summoner by name
+	 *
+	 * @returns Promise<Summoner | null> - information about a Summoner, if found; null otherwise
+	 */
+	async searchByName(searchKey: string): Promise<Summoner | null> {
+		const ctx = ' Summoner-Svc | searchByName '
+
+		try {
+			this.logger.verbose('Grabbing riotToken...', ctx)
+
+			const riotToken = this.configService.get<string>(
+				ENV_API_KEY,
+				ENV_API_KEY_DEFAULT,
+			)
+
+			this.logger.verbose(`riotToken="${riotToken}"`, ctx)
+			this.logger.debug('About to contact Riot API...', ctx)
+
+			const getResp: AxiosResponse<Summoner> = await this.httpService
+				.get(
+					`${SummonerService.BASE}/${SummonerService.ENDPOINT_SEARCH_BY_NAME}/${searchKey}`,
+					{
+						headers: {
+							'Accept-Charset':
+								'application/x-www-form-urlencoded; charset=UTF-8',
+							'Accept-Language': 'en-US,en;q=0.9',
+							'X-Riot-Token': riotToken,
+						},
+					},
+				)
+				.toPromise()
+
+			if (getResp.status === HttpStatus.NOT_FOUND) {
+				this.logger.debug('Summoner was not found, returning null...', ctx)
+				return null
+			}
+
+			return getResp.data
+		} catch (err) {
+			return null
+		}
+	}
+}
