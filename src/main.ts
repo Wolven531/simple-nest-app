c6a3881f6c61928ab9d071c6712151259ed8d0dc
wolven531 <anthony.williams.cs@gmail.com>
2021-05-27 19:23:06 -0600

    Log out the env vars

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(
+    'Loaded the following env vars',
+    `PORT=${process.env.PORT}`,
+    `RIOT_TOKEN=${process.env.RIOT_TOKEN}`,
+  );
+
+  const port = process.env.PORT || 8080;
+  // const riotToken = process.env.RIOT_TOKEN || '';
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 8080);
+
+  await app.listen(port);
 }
 bootstrap();
