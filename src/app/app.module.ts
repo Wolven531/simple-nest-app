a65f8dfc939667351b6e32d78dff75bff0f881b5
wolven531 <anthony.williams.cs@gmail.com>
2021-05-27 19:51:59 -0600

    Comment out other mods temp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
-import { number, object, string } from '@hapi/joi'
+import { object, string } from '@hapi/joi'
 import { HttpModule, Logger, Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 // in "aunt / uncle" dirs
 import { APIConfig, ConfigurationModule } from '../config'
-import { MasteryModule } from '../mastery/mastery.module'
-import { MatchlistModule } from '../matchlist/matchlist.module'
+// import { MasteryModule } from '../mastery/mastery.module'
+// import { MatchlistModule } from '../matchlist/matchlist.module'
 // services
 import { AppService } from '../services/app.service'
-import { JsonLoaderService } from '../services/json-loader.service'
-import { MatchlistService } from '../services/matchlist.service'
-import { StatsService } from '../services/stats.service'
-import { StatsModule } from '../stats/stats.module'
-import { UserModule } from '../user/user.module'
+// import { JsonLoaderService } from '../services/json-loader.service'
+// import { MatchlistService } from '../services/matchlist.service'
+// import { StatsService } from '../services/stats.service'
+// import { StatsModule } from '../stats/stats.module'
+// import { UserModule } from '../user/user.module'
 // local
 import { AppController } from './app.controller'
 
@@ -24,11 +24,11 @@ import { AppController } from './app.controller'
 			isGlobal: true,
 			validationSchema: object<APIConfig>({
 				// vals w/ defaults below
-				API_PORT: number().default(3050),
-				NODE_ENV: string()
-					.valid('development', 'production', 'test')
-					.default('development'),
-				SLC_API_DOCKER_TAG: string().default('v0.1'),
+				// API_PORT: number().default(3050),
+				// NODE_ENV: string()
+				// 	.valid('development', 'production', 'test')
+				// 	.default('development'),
+				// SLC_API_DOCKER_TAG: string().default('v0.1'),
 				// required below
 				RIOT_SECRET: string().required(),
 			}),
@@ -39,16 +39,16 @@ import { AppController } from './app.controller'
 		}),
 		HttpModule,
 		ConfigurationModule,
-		MasteryModule,
-		MatchlistModule,
-		StatsModule,
-		UserModule,
+		// MasteryModule,
+		// MatchlistModule,
+		// StatsModule,
+		// UserModule,
 	],
 	providers: [
 		AppService,
-		JsonLoaderService,
-		MatchlistService,
-		StatsService,
+		// JsonLoaderService,
+		// MatchlistService,
+		// StatsService,
 		Logger,
 	],
 })
