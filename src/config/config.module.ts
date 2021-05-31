e1fb6586b56f75d4ea17928169ffee06e50c95f5
wolven531 <anthony.williams.cs@gmail.com>
2021-05-30 21:28:12 -0600

    Add AppService to Config module

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,10 +1,11 @@
 import { HttpModule, Logger, Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { AppService } from '../services/app.service'
 import { ConfigController } from './config.controller'
 
 @Module({
 	controllers: [ConfigController],
 	imports: [HttpModule],
-	providers: [ConfigService, Logger],
+	providers: [AppService, ConfigService, Logger],
 })
 export class ConfigurationModule {}
