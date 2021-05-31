6546c722fdd8440b98e312ceeed7df5a82e880f6
wolven531 <anthony.williams.cs@gmail.com>
2021-05-31 16:51:14 -0600

    Fix failing tests

--- src/config/config.controller.spec.ts
+++ src/config/config.controller.spec.ts
@@ -1,12 +1,14 @@
 import { HttpModule, Logger } from '@nestjs/common'
-// import { ConfigService } from '@nestjs/config'
+import { ConfigService } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
 import { toggleMockedLogger } from '../../test/utils'
+import { ENV_API_KEY, ENV_API_SECRET_KEY } from '../constants'
 import { AppService } from '../services/app.service'
 import { ConfigController } from './config.controller'
 
 describe('ConfigController', () => {
-	// const fakeApiKey = 'some-api-key'
+	const fakeApiKey = 'some-api-key'
+	const fakeServerSecret = 'some-other-secret'
 	let controller: ConfigController
 	let testModule: TestingModule
 	let mockIsRiotTokenValid: jest.Mock
@@ -24,14 +26,22 @@ describe('ConfigController', () => {
 						isRiotTokenValid: mockIsRiotTokenValid,
 					}),
 				},
-				// {
-				// 	provide: ConfigService,
-				// 	useFactory: () => ({
-				// 		// NOTE: may need to udpate this mock logic when more config values are used
-				// 		// get: jest.fn((path, defaultVal) => fakeApiKey),
-				// 		get: jest.fn(() => fakeApiKey),
-				// 	}),
-				// },
+				{
+					provide: ConfigService,
+					useFactory: () => ({
+						// NOTE: may need to udpate this mock logic when more config values are used
+						get: jest.fn((path, defaultVal) => {
+							switch (path) {
+								case ENV_API_KEY:
+									return fakeApiKey
+								case ENV_API_SECRET_KEY:
+									return fakeServerSecret
+								default:
+									return ''
+							}
+						}),
+					}),
+				},
 				Logger,
 			],
 		}).compile()
@@ -59,7 +69,7 @@ describe('ConfigController', () => {
 			beforeEach(async () => {
 				mockIsRiotTokenValid.mockResolvedValue(fakeValidity)
 
-				resp = await controller.getConfig()
+				resp = await controller.getConfig(undefined)
 			})
 
 			it('returns object w/ riotTokenIsValid set according to mocked return from AppSvc', () => {
@@ -76,7 +86,7 @@ describe('ConfigController', () => {
 			beforeEach(async () => {
 				mockIsRiotTokenValid.mockResolvedValue(fakeValidity)
 
-				resp = await controller.getConfig()
+				resp = await controller.getConfig(undefined)
 			})
 
 			it('returns object w/ riotTokenIsValid set according to mocked return from AppSvc', () => {
