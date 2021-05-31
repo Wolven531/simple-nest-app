68361d068a46e9e07cba66abc6a923032ea0e6da
wolven531 <anthony.williams.cs@gmail.com>
2021-05-30 21:29:18 -0600

    Use AppService to check token validity

--- src/config/config.controller.ts
+++ src/config/config.controller.ts
@@ -6,28 +6,38 @@ import {
 	Inject,
 	Logger,
 } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
-import { ENV_API_KEY, ENV_API_KEY_DEFAULT } from '../constants'
+// import { ConfigService } from '@nestjs/config'
+import { AppService } from '../services/app.service'
+// import { ENV_API_KEY, ENV_API_KEY_DEFAULT } from '../constants'
 
 @Controller('config')
 export class ConfigController {
 	constructor(
-		@Inject(ConfigService)
-		private readonly configService: ConfigService,
+		@Inject(AppService)
+		private readonly appService: AppService,
+		// @Inject(ConfigService)
+		// private readonly configService: ConfigService,
 		@Inject(Logger)
 		private readonly logger: Logger,
 	) {}
 
 	@Get()
 	@HttpCode(HttpStatus.OK)
-	getConfig(): Record<string, string> {
+	async getConfig(): Promise<Record<string, unknown>> {
 		this.logger.verbose('GET request received', ' getConfig | Config-Ctrl ')
 
+		const riotTokenIsValid = await this.appService.isRiotTokenValid()
+
+		// const riotSecret = this.configService.get<string>(
+		// 	ENV_API_KEY,
+		// 	ENV_API_KEY_DEFAULT,
+		// )
+		// return {
+		// 	riotSecret,
+		// }
+
 		return {
-			riotSecret: this.configService.get<string>(
-				ENV_API_KEY,
-				ENV_API_KEY_DEFAULT,
-			),
+			riotTokenIsValid,
 		}
 	}
 }
