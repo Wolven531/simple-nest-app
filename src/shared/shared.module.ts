b7981739d3692d0b6bf6406f2d3466d89140cfe7
wolven531 <anthony.williams.cs@gmail.com>
2021-06-03 16:25:20 -0600

    Add HttpModule to Shared module

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { HttpModule, Logger, Module } from '@nestjs/common'
 import { AppService } from '../services/app.service'
 
 @Module({
-	exports: [AppService, Logger],
+	exports: [AppService, HttpModule, Logger],
 	imports: [HttpModule],
 	providers: [AppService, Logger],
 })
