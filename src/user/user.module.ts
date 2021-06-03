0dd5c45866d82763814983a2ac162c3584fed11e
wolven531 <anthony.williams.cs@gmail.com>
2021-06-02 20:16:13 -0600

    Fix runtime missing Provider error

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,19 @@
 import { HttpModule, Logger, Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { SummonerService } from '../services/summoner.service'
+import { AppService } from '../services/app.service'
 import { JsonLoaderService } from '../services/json-loader.service'
+import { SummonerService } from '../services/summoner.service'
 import { UserController } from './user.controller'
 
 @Module({
 	controllers: [UserController],
 	imports: [HttpModule],
-	providers: [ConfigService, JsonLoaderService, SummonerService, Logger],
+	providers: [
+		AppService,
+		ConfigService,
+		JsonLoaderService,
+		SummonerService,
+		Logger,
+	],
 })
 export class UserModule {}
