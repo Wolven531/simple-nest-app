c090d6b4dd7bd16ced18109c6ff4e91cca9515a8
wolven531 <anthony.williams.cs@gmail.com>
2021-06-02 20:43:44 -0600

    Add Shared module to User module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,19 +1,13 @@
 import { HttpModule, Logger, Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { AppService } from '../services/app.service'
 import { JsonLoaderService } from '../services/json-loader.service'
 import { SummonerService } from '../services/summoner.service'
+import { SharedModule } from '../shared/shared.module'
 import { UserController } from './user.controller'
 
 @Module({
 	controllers: [UserController],
-	imports: [HttpModule],
-	providers: [
-		AppService,
-		ConfigService,
-		JsonLoaderService,
-		SummonerService,
-		Logger,
-	],
+	imports: [HttpModule, SharedModule],
+	providers: [ConfigService, JsonLoaderService, SummonerService, Logger],
 })
 export class UserModule {}
