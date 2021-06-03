be864be1479494742bee84d15c2b6e267dd23041
wolven531 <anthony.williams.cs@gmail.com>
2021-06-03 16:32:34 -0600

    Move ConfigModule setup to Shared module from App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,4 @@
-import { object, string } from '@hapi/joi'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { APIConfig, ConfigurationModule } from '../config'
 import { SharedModule } from '../shared/shared.module'
 import { UserModule } from '../user/user.module'
 // import { MasteryModule } from '../mastery/mastery.module'
@@ -17,32 +14,11 @@ import { AppController } from './app.controller'
 @Module({
 	controllers: [AppController],
 	imports: [
-		// NOTE: see https://docs.nestjs.com/techniques/configuration for more info
-		ConfigModule.forRoot({
-			envFilePath: ['.env.production', '.env.development', '.env'],
-			isGlobal: true,
-			validationSchema: object<APIConfig>({
-				// vals w/ defaults below
-				// API_PORT: number().default(3050),
-				// NODE_ENV: string()
-				// 	.valid('development', 'production', 'test')
-				// 	.default('development'),
-				// SLC_API_DOCKER_TAG: string().default('v0.1'),
-				// required below
-				RIOT_SECRET: string().required(),
-				SERVER_SECRET: string().required(),
-			}),
-			validationOptions: {
-				abortEarly: false,
-				allowUnknown: true,
-			},
-		}),
-		ConfigurationModule,
+		SharedModule,
+		UserModule,
 		// MasteryModule,
 		// MatchlistModule,
 		// StatsModule,
-		SharedModule,
-		UserModule,
 	],
 	providers: [
 		// JsonLoaderService,
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,9 +1,36 @@
+import { object, string } from '@hapi/joi'
 import { HttpModule, Logger, Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { APIConfig } from '../config'
 import { AppService } from '../services/app.service'
 
 @Module({
-	exports: [AppService, HttpModule, Logger],
-	imports: [HttpModule],
+	exports: [AppService, ConfigModule, HttpModule, Logger],
+	imports: [
+		// NOTE: see https://docs.nestjs.com/techniques/configuration for more info
+		ConfigModule.forRoot({
+			envFilePath: ['.env.production', '.env.development', '.env'],
+			isGlobal: true,
+			validationSchema: object<APIConfig>({
+				// vals w/ defaults below
+				// API_PORT: number().default(3050),
+				// NODE_ENV: string()
+				// 	.valid('development', 'production', 'test')
+				// 	.default('development'),
+				// SLC_API_DOCKER_TAG: string().default('v0.1'),
+				// required below
+				RIOT_SECRET: string().required(),
+				SERVER_SECRET: string().required(),
+			}),
+			validationOptions: {
+				abortEarly: false,
+				allowUnknown: true,
+			},
+		}),
+		// TODO - does ConfigurationModule need to be imported ???
+		// ConfigurationModule,
+		HttpModule,
+	],
 	providers: [AppService, Logger],
 })
 export class SharedModule {}
