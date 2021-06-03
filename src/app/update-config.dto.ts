ac5a9d9f78216c82dd9d26d2f0491c903717d6dc
wolven531 <anthony.williams.cs@gmail.com>
2021-06-02 20:15:43 -0600

    Add Config entity and DTO classes

---
+++ src/app/update-config.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty, PartialType } from '@nestjs/swagger'
+import { CreateConfigDto } from './create-config.dto'
+
+export class UpdateConfigDto extends PartialType(CreateConfigDto) {
+	/**
+	 * @param secret - string
+	 * @param token - string
+	 */
+	constructor(secret: string, token: string) {
+		super()
+
+		this.secret = secret
+		this.token = token
+	}
+
+	@ApiProperty({
+		description:
+			'Value used to authenticate the request; should match $env:SERVER_SECRET',
+		name: 'secret',
+		nullable: false,
+		required: true,
+		type: String,
+	})
+	secret: string
+
+	@ApiProperty({
+		description:
+			'New value for the Riot API token; will override $env:RIOT_SECRET',
+		name: 'token',
+		nullable: false,
+		required: true,
+		type: String,
+	})
+	token: string
+}
