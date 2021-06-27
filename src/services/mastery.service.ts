a97798d8f5046b006df23e67b2fb319155d32d20
wolven531 <anthony.williams.cs@gmail.com>
2021-06-26 18:30:00 -0600

    Disable all logic but getMasteryTotal

--- src/services/mastery.service.ts
+++ src/services/mastery.service.ts
@@ -1,14 +1,14 @@
-import { User } from '../models/user.model'
+// import { User } from '../models/user.model'
 import {
 	HttpService,
 	Inject,
 	Injectable,
 	Logger,
-	NotFoundException,
+	// NotFoundException,
 } from '@nestjs/common'
-import { utc } from 'moment'
+// import { utc } from 'moment'
 import { DEFAULT_TOTAL_MASTERY_SCORE, REGION } from '../constants'
-import { JsonLoaderService } from './json-loader.service'
+// import { JsonLoaderService } from './json-loader.service'
 
 @Injectable()
 export class MasteryService {
@@ -17,7 +17,6 @@ export class MasteryService {
 		private readonly httpService: HttpService,
 		@Inject(Logger)
 		private readonly logger: Logger,
-		private readonly jsonLoaderService: JsonLoaderService,
 	) {}
 
 	/**
@@ -38,27 +37,27 @@ export class MasteryService {
 		summonerId: string,
 		defaultMasteryTotal = DEFAULT_TOTAL_MASTERY_SCORE,
 	): Promise<number> {
-		const loadedUsers = this.jsonLoaderService.loadUsersFromFile()
-		const targetUser = loadedUsers.find(
-			(user) => user.summonerId === summonerId,
-		)
+		// const loadedUsers = this.jsonLoaderService.loadUsersFromFile()
+		// const targetUser = loadedUsers.find(
+		// 	(user) => user.summonerId === summonerId,
+		// )
 
-		if (!targetUser) {
-			this.logger.error(
-				new NotFoundException(
-					`User w/ summonerId="${summonerId}" was not found`,
-				),
-			)
-			return Promise.resolve(defaultMasteryTotal)
-		}
+		// if (!targetUser) {
+		// 	this.logger.error(
+		// 		new NotFoundException(
+		// 			`User w/ summonerId="${summonerId}" was not found`,
+		// 		),
+		// 	)
+		// 	return Promise.resolve(defaultMasteryTotal)
+		// }
 
-		if (targetUser.isFresh) {
-			this.logger.log(
-				`loaded from cache totalScore=${targetUser.masteryTotal}`,
-				' getMasteryTotal | match-svc ',
-			)
-			return Promise.resolve(targetUser.masteryTotal)
-		}
+		// if (targetUser.isFresh) {
+		// 	this.logger.log(
+		// 		`loaded from cache totalScore=${targetUser.masteryTotal}`,
+		// 		' getMasteryTotal | match-svc ',
+		// 	)
+		// 	return Promise.resolve(targetUser.masteryTotal)
+		// }
 
 		return this.httpService
 			.get(
@@ -106,52 +105,52 @@ export class MasteryService {
 	 * @returns Promise<User[]> Attempt to fetch over HTTP the total mastery score for each user in
 	 *   the users file; it then returns the updated Users
 	 */
-	refreshMasteryTotalForAllUsers(apiKey: string): Promise<User[]> {
-		const loadedUsers = this.jsonLoaderService.loadUsersFromFile()
+	// refreshMasteryTotalForAllUsers(apiKey: string): Promise<User[]> {
+	// 	const loadedUsers = this.jsonLoaderService.loadUsersFromFile()
 
-		return Promise.all(
-			loadedUsers.map((user) =>
-				this.httpService
-					.get(
-						`https://${REGION}.api.riotgames.com/lol/champion-mastery/v4/scores/by-summoner/${user.summonerId}`,
-						{
-							headers: {
-								'Accept-Charset':
-									'application/x-www-form-urlencoded; charset=UTF-8',
-								'Accept-Language': 'en-US,en;q=0.9',
-								'X-Riot-Token': apiKey,
-							},
-						},
-					)
-					.toPromise()
-					.then((resp) => {
-						const masteryTotalScore = parseInt(resp.data, 10)
-						const utcNow = utc()
+	// 	return Promise.all(
+	// 		loadedUsers.map((user) =>
+	// 			this.httpService
+	// 				.get(
+	// 					`https://${REGION}.api.riotgames.com/lol/champion-mastery/v4/scores/by-summoner/${user.summonerId}`,
+	// 					{
+	// 						headers: {
+	// 							'Accept-Charset':
+	// 								'application/x-www-form-urlencoded; charset=UTF-8',
+	// 							'Accept-Language': 'en-US,en;q=0.9',
+	// 							'X-Riot-Token': apiKey,
+	// 						},
+	// 					},
+	// 				)
+	// 				.toPromise()
+	// 				.then((resp) => {
+	// 					const masteryTotalScore = parseInt(resp.data, 10)
+	// 					const utcNow = utc()
 
-						this.logger.log(
-							`fetched total mastery over HTTP masteryTotalScore=${masteryTotalScore}`,
-							' refreshMasteryTotalForAllUsers | match-svc ',
-						)
+	// 					this.logger.log(
+	// 						`fetched total mastery over HTTP masteryTotalScore=${masteryTotalScore}`,
+	// 						' refreshMasteryTotalForAllUsers | match-svc ',
+	// 					)
 
-						user.lastUpdated = utcNow.valueOf()
-						user.masteryTotal = masteryTotalScore
+	// 					user.lastUpdated = utcNow.valueOf()
+	// 					user.masteryTotal = masteryTotalScore
 
-						return user
-					}),
-			),
-		)
-			.then((updatedUsers) => {
-				this.jsonLoaderService.updateUsersFile(updatedUsers)
+	// 					return user
+	// 				}),
+	// 		),
+	// 	)
+	// 		.then((updatedUsers) => {
+	// 			this.jsonLoaderService.updateUsersFile(updatedUsers)
 
-				return updatedUsers
-			})
-			.catch((err) => {
-				this.logger.error(
-					`Error while updating users!\n\n${JSON.stringify(err, null, 4)}`,
-					' refreshMasteryTotalForAllUsers | match-svc ',
-				)
+	// 			return updatedUsers
+	// 		})
+	// 		.catch((err) => {
+	// 			this.logger.error(
+	// 				`Error while updating users!\n\n${JSON.stringify(err, null, 4)}`,
+	// 				' refreshMasteryTotalForAllUsers | match-svc ',
+	// 			)
 
-				return loadedUsers
-			})
-	}
+	// 			return loadedUsers
+	// 		})
+	// }
 }
