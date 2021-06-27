03914bf286774a0a8db600f2e2c1047f3a43ccfa
wolven531 <anthony.williams.cs@gmail.com>
2021-06-26 18:33:33 -0600

    Import SharedModule

--- src/mastery/mastery.module.ts
+++ src/mastery/mastery.module.ts
@@ -1,17 +1,18 @@
 import { HttpModule, Logger, Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { JsonLoaderService } from '../services/json-loader.service'
-import { MatchlistService } from '../services/matchlist.service'
+// import { JsonLoaderService } from '../services/json-loader.service'
+// import { MatchlistService } from '../services/matchlist.service'
 import { MasteryService } from '../services/mastery.service'
+import { SharedModule } from '../shared/shared.module'
 import { MasteryController } from './mastery.controller'
 
 @Module({
 	controllers: [MasteryController],
-	imports: [HttpModule],
+	imports: [HttpModule, SharedModule],
 	providers: [
 		ConfigService,
-		JsonLoaderService,
-		MatchlistService,
+		// JsonLoaderService,
+		// MatchlistService,
 		MasteryService,
 		Logger,
 	],
