ec931f8a91db30c078fdf13823d9842ddef7c26c
wolven531 <anthony.williams.cs@gmail.com>
2021-07-12 22:02:58 -0600

    Add boilerplate for withUser - mastery total

--- src/mastery/mastery.controller.ts
+++ src/mastery/mastery.controller.ts
@@ -7,8 +7,15 @@ import {
 	Inject,
 	Logger,
 	Param,
+	Query,
 } from '@nestjs/common'
-import { ApiExtraModels, ApiOperation, ApiTags } from '@nestjs/swagger'
+import {
+	ApiExtraModels,
+	ApiOperation,
+	ApiParam,
+	ApiQuery,
+	ApiTags,
+} from '@nestjs/swagger'
 import { AppService } from '../services/app.service'
 import { MasteryService } from '../services/mastery.service'
 
@@ -36,14 +43,51 @@ export class MasteryController {
 		summary: 'Get total mastery score for a given summonerId',
 		tags: ['mastery', 'summoner', 'summonerId', 'total'],
 	})
+	@ApiParam({
+		name: 'summonerId',
+		type: 'string',
+		required: true,
+		allowEmptyValue: false,
+		description: 'Summoner ID to user during lookup',
+		examples: {
+			'Custom Summoner ID': {
+				value: '',
+			},
+			'Summoner ID for 0NeveroDDoreveN0': {
+				value: 'jzbq0gSuHosYXo4yk1oi0Cs432As65H-0xyaIG2qZuuVi_iY',
+			},
+		},
+		style: 'simple',
+	})
+	@ApiQuery({
+		name: 'withUser',
+		type: 'boolean',
+		required: false,
+		allowEmptyValue: false,
+		description:
+			'If true, return user data alongside number; otherwise, return simple number alone',
+		examples: {
+			'Default (not specified)': {
+				value: undefined,
+			},
+			'Request with user data (withUser=true)': {
+				value: true,
+			},
+			'Request without user data (withUser=false)': {
+				value: false,
+			},
+		},
+		style: 'simple',
+	})
 	@ApiTags('mastery', 'total')
 	@HttpCode(HttpStatus.OK)
 	@Header('Cache-Control', 'none')
 	async getMasteryTotal(
 		@Param('summonerId') summonerId: string,
+		@Query('withUser') withUser = false,
 	): Promise<number> {
 		this.logger.log(
-			`summonerId=${summonerId}`,
+			`summonerId="${summonerId}" withUser=${withUser}`,
 			' getMasteryTotal | MatchlistCtrl ',
 		)
 		const apiKey = this.appService.getRiotToken()
