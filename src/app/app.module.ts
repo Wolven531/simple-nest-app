4a3280719bb8c5b387ce707825bb809481669405
wolven531 <anthony.williams.cs@gmail.com>
2021-07-13 19:13:07 -0600

    Enable matchlist controller

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common'
 import { ConfigurationModule } from '../config'
 import { HealthModule } from '../health/health.module'
 import { MasteryModule } from '../mastery/mastery.module'
+import { MatchlistModule } from '../matchlist/matchlist.module'
 import { SharedModule } from '../shared/shared.module'
 import { UserModule } from '../user/user.module'
 // local
@@ -14,6 +15,7 @@ import { AppController } from './app.controller'
 		ConfigurationModule, // contains config endpoints
 		HealthModule, // contains health endpoints
 		MasteryModule, // contains user endpoints
+		MatchlistModule, // contains matchlist endpoints
 		UserModule, // contains user endpoints
 	],
 	providers: [],
--- src/matchlist/matchlist.module.ts
+++ src/matchlist/matchlist.module.ts
@@ -1,12 +1,12 @@
 import { HttpModule, Logger, Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { JsonLoaderService } from '../services/json-loader.service'
 import { MatchlistService } from '../services/matchlist.service'
+import { SharedModule } from '../shared/shared.module'
 import { MatchlistController } from './matchlist.controller'
 
 @Module({
 	controllers: [MatchlistController],
-	imports: [HttpModule],
-	providers: [ConfigService, JsonLoaderService, MatchlistService, Logger],
+	imports: [HttpModule, SharedModule],
+	providers: [ConfigService, MatchlistService, Logger],
 })
 export class MatchlistModule {}
