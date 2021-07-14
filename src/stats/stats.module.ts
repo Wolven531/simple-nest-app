916fb618b89cc7b1d7558ade30622ae803512c5a
wolven531 <anthony.williams.cs@gmail.com>
2021-07-13 19:22:57 -0600

    Fix imports for stats module

--- src/stats/stats.module.ts
+++ src/stats/stats.module.ts
@@ -1,19 +1,13 @@
 import { HttpModule, Logger, Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { JsonLoaderService } from '../services/json-loader.service'
 import { MatchlistService } from '../services/matchlist.service'
 import { StatsService } from '../services/stats.service'
+import { SharedModule } from '../shared/shared.module'
 import { StatsController } from './stats.controller'
 
 @Module({
 	controllers: [StatsController],
-	imports: [HttpModule],
-	providers: [
-		ConfigService,
-		JsonLoaderService,
-		MatchlistService,
-		StatsService,
-		Logger,
-	],
+	imports: [HttpModule, SharedModule],
+	providers: [ConfigService, MatchlistService, StatsService, Logger],
 })
 export class StatsModule {}
