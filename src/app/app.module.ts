277112586c87ec1ba9d0672a5adaf230b72ae434
wolven531 <anthony.williams.cs@gmail.com>
2021-07-14 18:25:26 -0600

    Temp disable matchlist service dependents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common'
 import { ConfigurationModule } from '../config'
 import { HealthModule } from '../health/health.module'
 import { MasteryModule } from '../mastery/mastery.module'
-import { MatchlistModule } from '../matchlist/matchlist.module'
+// import { MatchlistModule } from '../matchlist/matchlist.module'
 import { SharedModule } from '../shared/shared.module'
-import { StatsModule } from '../stats/stats.module'
+// import { StatsModule } from '../stats/stats.module'
 import { UserModule } from '../user/user.module'
-// local
 import { AppController } from './app.controller'
 
 @Module({
@@ -16,8 +15,9 @@ import { AppController } from './app.controller'
 		ConfigurationModule, // contains config endpoints
 		HealthModule, // contains health endpoints
 		MasteryModule, // contains user endpoints
-		MatchlistModule, // contains matchlist endpoints
-		StatsModule, // contains stats endpoints
+		// TODO - below not available until Riot makes match v5 API available to production API keys
+		// MatchlistModule, // contains matchlist endpoints
+		// StatsModule, // contains stats endpoints
 		UserModule, // contains user endpoints
 	],
 	providers: [],
