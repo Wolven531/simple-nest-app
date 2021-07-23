7e0a52eee600db37b2a54ab953db923d2a9bdd33
wolven531 <anthony.williams.cs@gmail.com>
2021-07-22 22:19:33 -0600

    Enable Matchlist module again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,8 @@ import { ConfigurationModule } from '../config'
 import { HealthModule } from '../health'
 import { HttpRequestInterceptor } from '../http-request.interceptor'
 import { MasteryModule } from '../mastery'
-// import { MatchlistModule } from '../matchlist'
-import { SharedModule } from '../shared/shared.module'
+import { MatchlistModule } from '../matchlist'
+import { SharedModule } from '../shared'
 // import { StatsModule } from '../stats'
 import { UserModule } from '../user'
 import { AppController } from './app.controller'
@@ -22,8 +22,8 @@ import { AppController } from './app.controller'
 		ConfigurationModule, // contains config endpoints
 		HealthModule, // contains health endpoints
 		MasteryModule, // contains user endpoints
+		MatchlistModule, // contains matchlist endpoints
 		// TODO - below not available until Riot makes match v5 API available to production API keys
-		// MatchlistModule, // contains matchlist endpoints
 		// StatsModule, // contains stats endpoints
 		UserModule, // contains user endpoints
 	],
