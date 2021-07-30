109ddb2cd85fc5445c3f0b938bc6f26e850a9722
wolven531 <anthony.williams.cs@gmail.com>
2021-07-29 23:42:54 -0600

    Fix compile error

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,7 @@
 import { HttpModule } from '@nestjs/axios'
 import { Logger, Module } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { MasteryService } from 'src/services/mastery.service'
 import { SummonerService } from '../services/summoner.service'
 import { UserService } from '../services/user.service'
 import { SharedModule } from '../shared'
@@ -9,6 +10,12 @@ import { UserController } from './user.controller'
 @Module({
 	controllers: [UserController],
 	imports: [HttpModule, SharedModule],
-	providers: [ConfigService, SummonerService, UserService, Logger],
+	providers: [
+		ConfigService,
+		MasteryService,
+		SummonerService,
+		UserService,
+		Logger,
+	],
 })
 export class UserModule {}
