05e448752b73d28db760c3348383182e2ed0e113
wolven531 <anthony.williams.cs@gmail.com>
2021-08-03 23:03:38 -0600

    Add queue param to stats endpoint

--- src/stats/stats.controller.spec.ts
+++ src/stats/stats.controller.spec.ts
@@ -2,22 +2,30 @@ import { HttpModule } from '@nestjs/axios'
 import { BadRequestException, Logger } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
 import { toggleMockedLogger } from '../../test/utils'
+import { COMMON_QUEUE_TYPES } from '../constants'
 import { CalculatedStats } from '../models/calculated-stats.model'
 import { Game } from '../models/game.model'
+import { Match } from '../models/match.model'
 import { MatchlistService } from '../services/matchlist.service'
 import { StatsService } from '../services/stats.service'
 import { StatsController } from './stats.controller'
 
 describe('StatsController', () => {
+	// constants for testing
 	const fakeGame: Game = {} as Game
+	const fakeGames: Game[] = []
+
+	// standard testing setup
 	let controller: StatsController
 	let testModule: TestingModule
-	let mockGetGame: jest.Mock
+
+	// mocks for testing
+	// let mockGetGame: jest.Mock
 	let mockGetMatchlist: jest.Mock
 
 	beforeEach(async () => {
-		mockGetGame = jest.fn().mockResolvedValue(fakeGame)
-		mockGetMatchlist = jest.fn().mockResolvedValue([])
+		// mockGetGame = jest.fn().mockResolvedValue(fakeGame)
+		mockGetMatchlist = jest.fn().mockResolvedValue(fakeGames)
 
 		testModule = await Test.createTestingModule({
 			controllers: [StatsController],
@@ -27,11 +35,19 @@ describe('StatsController', () => {
 					provide: MatchlistService,
 					useFactory: () =>
 						({
-							v4GetGame: mockGetGame,
+							// v4GetGame: mockGetGame,
 							v4GetMatchlist: mockGetMatchlist,
 						} as unknown as MatchlistService),
 				},
-				StatsService,
+				// {
+				// 	provide: MatchlistService,
+				// 	useFactory: () =>
+				// 		({
+				// 			// v4GetGame: mockGetGame,
+				// 			v4GetMatchlist: mockGetMatchlist,
+				// 		} as unknown as MatchlistService),
+				// },
+				StatsService, // using actual service
 				Logger,
 			],
 		}).compile()
@@ -58,7 +74,11 @@ describe('StatsController', () => {
 
 			beforeEach(async () => {
 				try {
-					resp = await controller.getSummaryForAccountId('', undefined)
+					resp = await controller.getSummaryForAccountId(
+						'',
+						undefined,
+						undefined,
+					)
 				} catch (err) {
 					capturedError = err
 				}
@@ -82,24 +102,29 @@ describe('StatsController', () => {
 		})
 
 		describe('invoke getSummaryForAccountId("someAccountId", undefined)', () => {
+			const fakeAccountId = 'someAccountId'
 			const fakeKDA = 3.14
+			const fakeQueueFilter: keyof typeof COMMON_QUEUE_TYPES = 'aram'
 			let capturedError: Error
 			let mockCalculateGeneralStats: jest.Mock
 			let resp: CalculatedStats
 
 			beforeEach(async () => {
 				mockCalculateGeneralStats = jest.fn(
-					(targetAccountKey, games) => ({ kDA: fakeKDA } as CalculatedStats),
+					(targetAccountKey, games) =>
+						({ kDA: fakeKDA } as CalculatedStats),
 				)
 
-				jest
-					.spyOn(testModule.get(StatsService), 'calculateGeneralStats')
-					.mockImplementationOnce(mockCalculateGeneralStats)
+				jest.spyOn(
+					testModule.get(StatsService),
+					'calculateGeneralStats',
+				).mockImplementationOnce(mockCalculateGeneralStats)
 
 				try {
 					resp = await controller.getSummaryForAccountId(
-						'someAccountId',
+						fakeAccountId,
 						undefined,
+						fakeQueueFilter,
 					)
 				} catch (err) {
 					capturedError = err
@@ -109,7 +134,17 @@ describe('StatsController', () => {
 			it('invokes MatchlistSvc.getMatchlist() and StatsSvc.calculateGeneralStats(), does NOT throw error', () => {
 				expect(capturedError).toBeUndefined()
 				expect(mockCalculateGeneralStats).toHaveBeenCalledTimes(1)
+				expect(mockCalculateGeneralStats).toHaveBeenLastCalledWith(
+					fakeAccountId,
+					fakeGames,
+				)
 				expect(mockGetMatchlist).toHaveBeenCalledTimes(1)
+				expect(mockGetMatchlist).toHaveBeenLastCalledWith(
+					fakeAccountId,
+					10, // from endpoint default
+					true,
+					fakeQueueFilter,
+				)
 				expect(resp).toMatchObject({ kDA: fakeKDA } as CalculatedStats)
 			})
 		})
