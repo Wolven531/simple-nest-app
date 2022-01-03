d1290e95ae65b3cbd629628f2917dea13da8438a
wolven531 <anthony.williams.cs@gmail.com>
2022-01-02 21:00:14 -0700

    Add mastery svc to user resolver

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,15 +1,37 @@
-import { Inject } from '@nestjs/common'
+import { Inject, Logger } from '@nestjs/common'
 import { Query, Resolver } from '@nestjs/graphql'
 import { User } from '../models/user.model'
+import { MasteryService } from '../services/mastery.service'
 import { UserService } from './user.service'
 
 @Resolver((of) => User)
 export class UserResolver {
-	constructor(@Inject(UserService) private userService: UserService) {}
+	constructor(
+		@Inject(MasteryService)
+		private readonly masteryService: MasteryService,
+		@Inject(UserService)
+		private readonly userService: UserService,
+		@Inject(Logger)
+		private readonly logger: Logger,
+	) {}
 
 	@Query((returns) => [User])
 	async users(): Promise<User[]> {
-		return this.userService.users
+		this.logger.debug('', ' User-Resolver | getUsers ')
+
+		const updatedUsers = Promise.all(
+			this.userService.users.map(async (user) => {
+				const masteryTotal = await this.masteryService.getMasteryTotal(
+					user.summonerId,
+				)
+
+				user.masteryTotal = masteryTotal
+
+				return user
+			}),
+		)
+
+		return updatedUsers
 	}
 
 	//   @Query(returns => User)
