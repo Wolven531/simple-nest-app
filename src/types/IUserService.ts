82927368059b48d818e10724a2fb000bacb0e135
wolven531 <anthony.williams.cs@gmail.com>
2022-01-03 22:39:17 -0700

    Add IUserService type

---
+++ src/types/IUserService.ts
@@ -0,0 +1,16 @@
+import { User } from '../models/user.model'
+
+export interface IUserService {
+	// NOTE - current interface
+	addUser: (newUser: User) => void
+	getUserByFriendlyName: (friendlyName: string) => Promise<User | undefined>
+	getUsers: () => Promise<User[]>
+	setup: (data?: User[]) => void
+
+	// NOTE - future / async methods
+	// addUser: (newUser: User) => Promise<void>
+	// setup: (data?: User[]) => Promise<void>
+
+	// NOTE - former property
+	// get users(): User[]
+}
