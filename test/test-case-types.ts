43d560180b369cceb1273123e08ac6e555a8494e
wolven531 <anthony.williams.cs@gmail.com>
2022-01-09 13:33:25 -0700

    Move test case types to separate file

---
+++ test/test-case-types.ts
@@ -0,0 +1,24 @@
+import { COMMON_QUEUE_TYPES } from '../src/constants'
+import { GameV5 } from '../src/models/game-v5.model'
+
+export type TestCase_GetGame = {
+	description: string
+	expectedCountHttpGet: number
+	expectedResult: GameV5 | null
+	mockHttpGet: jest.Mock
+	paramGameId: string
+}
+
+export type TestCase_GetMatchlist = {
+	description: string
+	expectedCountHttpGet: number
+	expectedCountGetGame: number
+	expectedUrlParamCount: number
+	expectedUrlParamQueueFilter: string
+	expectedResult: GameV5[]
+	mockGetGame: jest.Mock
+	mockHttpGet: jest.Mock
+	paramPuuid: string
+	paramGetLastX: number | undefined
+	paramQueueType: keyof typeof COMMON_QUEUE_TYPES | undefined
+}
