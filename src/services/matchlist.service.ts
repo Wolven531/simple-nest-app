0be333dc047531ca65e6736d12650f2a0ee98047
wolven531 <anthony.williams.cs@gmail.com>
2022-01-08 19:50:09 -0700

    Add v5GetMatchlist, v5GetGame; deprecate v4

--- src/services/matchlist.service.ts
+++ src/services/matchlist.service.ts
@@ -24,12 +24,134 @@ export class MatchlistService {
 	) {}
 
 	/**
+	 * This method uses the Riot Match API v5 to retrieve a Game
+	 *
+	 * @param gameId string Identifier for game to retrieve
+	 * @returns Promise<Game> if successful; Promise<null> otherwise
+	 */
+	async v5GetGame(gameId: string): Promise<Game | null> {
+		const apiKey = this.appService.getRiotToken()
+
+		return firstValueFrom(
+			this.httpService.get(
+				`https://${REGION}.api.riotgames.com/lol/match/v5/matches/${gameId}`,
+				{
+					headers: {
+						'Accept-Charset':
+							'application/x-www-form-urlencoded; charset=UTF-8',
+						'Accept-Language': 'en-US,en;q=0.9',
+						'X-Riot-Token': apiKey,
+					},
+				},
+			),
+		)
+			.then<Game>((resp) => {
+				const game = resp.data as Game
+
+				this.logger.log(
+					`Game w/ ID "${game.gameId}" loaded`,
+					' getGame | match-svc ',
+				)
+
+				return game
+			})
+			.catch((err) => {
+				this.logger.error(
+					`Error while fetching game!\n\n${JSON.stringify(
+						err,
+						null,
+						4,
+					)}`,
+					' getGame | match-svc ',
+				)
+
+				return null
+			})
+	}
+
+	/**
+	 * This method uses the Riot Match API v5 to retrieve a list of matches for a specific Summoner
+	 *
+	 * @param puuid string `puuid` of a Summoner to use when retrieving matches
+	 * @param getLastX number Defaults to 10; number of matches to retrieve
+	 * @param queueType string Defaults to undefined; can be specified as a key from COMMON_QUEUE_TYPES
+	 *     to filter which matches to request
+	 * @returns A collection of Game objects
+	 */
+	async v5GetMatchlist(
+		puuid: string,
+		getLastX = 10,
+		queueType: keyof typeof COMMON_QUEUE_TYPES = undefined,
+	): Promise<Game[]> {
+		const apiKey = this.appService.getRiotToken()
+
+		// update value BEFORE hitting Riot API
+		if (getLastX < MIN_NUM_MATCHES) {
+			getLastX = MIN_NUM_MATCHES
+		}
+
+		if (getLastX > MAX_NUM_MATCHES) {
+			getLastX = MAX_NUM_MATCHES
+		}
+
+		const filterQueue =
+			queueType === undefined
+				? ''
+				: `&queue=${COMMON_QUEUE_TYPES[queueType].id}`
+
+		return firstValueFrom(
+			this.httpService.get(
+				`https://${REGION}.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?count=${getLastX}${filterQueue}`,
+				{
+					headers: {
+						'Accept-Charset':
+							'application/x-www-form-urlencoded; charset=UTF-8',
+						'Accept-Language': 'en-US,en;q=0.9',
+						'X-Riot-Token': apiKey,
+					},
+				},
+			),
+		)
+			.then<string[]>((resp) => {
+				const matchIds = resp.data as string[]
+
+				this.logger.log(
+					`${matchIds.length} total matches`,
+					' getMatchlist | match-svc ',
+				)
+
+				return matchIds
+			})
+			.then<Game[]>((allMatches: string[]) => {
+				this.logger.log(
+					`retrieving additional info for ${allMatches.length} indiviudal games...`,
+					' getMatchlist | match-svc ',
+				)
+
+				return Promise.all(allMatches.map(this.v5GetGame))
+			})
+			.catch((err) => {
+				this.logger.error(
+					`Error while fetching matches!\n\n${JSON.stringify(
+						err,
+						null,
+						4,
+					)}`,
+					' getMatchlist | match-svc ',
+				)
+
+				return []
+			})
+	}
+
+	/**
+	 * @deprecated
 	 * This method uses the Riot Match API v4 to retrieve a Game
 	 *
 	 * @param gameId number Identifier for game to retrieve
 	 * @returns Promise<Game> if successful; Promise<null> otherwise
 	 */
-	v4GetGame(gameId: number): Promise<Game | null> {
+	async v4GetGame(gameId: number): Promise<Game | null> {
 		const apiKey = this.appService.getRiotToken()
 
 		return firstValueFrom(
@@ -70,6 +192,7 @@ export class MatchlistService {
 	}
 
 	/**
+	 * @deprecated
 	 * This method uses the Riot Match API v4 to retrieve a list of matches for a specific Summoner
 	 *
 	 * @param accountId string `accountId` of a Summoner to use when retrieving matches
@@ -80,7 +203,7 @@ export class MatchlistService {
 	 *     to filter which matches to request
 	 * @returns A collection of either Match objects (default) or Game objects (includeGameData=true)
 	 */
-	v4GetMatchlist(
+	async v4GetMatchlist(
 		accountId: string,
 		getLastX = 10,
 		includeGameData = false,
