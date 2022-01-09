d0114e30bc8ac8d36ef3a04a41255a5ca777383b
wolven531 <anthony.williams.cs@gmail.com>
2022-01-09 15:03:31 -0700

    Parse resp.data.info properly; remove v4

--- src/services/matchlist.service.ts
+++ src/services/matchlist.service.ts
@@ -5,13 +5,9 @@ import {
 	COMMON_QUEUE_TYPES,
 	MAX_NUM_MATCHES,
 	MIN_NUM_MATCHES,
-	REGION,
 	REGION_V5,
 } from '../constants'
-import { GameV5 } from '../models/game-v5.model'
-import { Game } from '../models/game.model'
-import { Match } from '../models/match.model'
-import { Matchlist } from '../models/matchlist.model'
+import { GameV5 } from '../models/v5/game-v5.model'
 import { AppService } from './app.service'
 
 @Injectable()
@@ -34,6 +30,11 @@ export class MatchlistService {
 	async v5GetGame(gameId: string): Promise<GameV5 | null> {
 		const apiKey = this.appService.getRiotToken()
 
+		this.logger.log(
+			`About to fetch game (id = ${gameId})`,
+			' getGame | match-svc ',
+		)
+
 		return firstValueFrom(
 			this.httpService.get(
 				`https://${REGION_V5}.api.riotgames.com/lol/match/v5/matches/${gameId}`,
@@ -48,10 +49,10 @@ export class MatchlistService {
 			),
 		)
 			.then<GameV5>((resp) => {
-				const game = resp.data as GameV5
+				const game = resp.data.info as GameV5
 
 				this.logger.log(
-					`Game w/ ID "${game.gameId}" loaded`,
+					`Game loaded\n\n${JSON.stringify(game, null, 4)}\n\n`,
 					' getGame | match-svc ',
 				)
 
@@ -101,6 +102,11 @@ export class MatchlistService {
 				? ''
 				: `&queue=${COMMON_QUEUE_TYPES[queueType].id}`
 
+		this.logger.log(
+			`About to use HTTP to grab list of game IDs for puuid="${puuid}"`,
+			' getMatchlist | match-svc ',
+		)
+
 		return firstValueFrom(
 			this.httpService.get(
 				`https://${REGION_V5}.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?count=${getLastX}${filterQueue}`,
@@ -136,146 +142,6 @@ export class MatchlistService {
 						return this.v5GetGame(matchId)
 					}),
 				)
-			})
-			.catch((err) => {
-				this.logger.error(
-					`Error while fetching matches!\n\n${JSON.stringify(
-						err,
-						null,
-						4,
-					)}`,
-					' getMatchlist | match-svc ',
-				)
-
-				return []
-			})
-	}
-
-	/**
-	 * @deprecated
-	 * This method uses the Riot Match API v4 to retrieve a Game
-	 *
-	 * @param gameId number Identifier for game to retrieve
-	 * @returns Promise<Game> if successful; Promise<null> otherwise
-	 */
-	async v4GetGame(gameId: number): Promise<Game | null> {
-		const apiKey = this.appService.getRiotToken()
-
-		return firstValueFrom(
-			this.httpService.get(
-				`https://${REGION}.api.riotgames.com/lol/match/v4/matches/${gameId}`,
-				{
-					headers: {
-						'Accept-Charset':
-							'application/x-www-form-urlencoded; charset=UTF-8',
-						'Accept-Language': 'en-US,en;q=0.9',
-						'X-Riot-Token': apiKey,
-					},
-				},
-			),
-		)
-			.then((resp) => {
-				const gameInfo: Game = resp.data
-
-				this.logger.log(
-					`Fetched game (id = ${gameId})! Created = ${gameInfo.gameCreation} Duration = ${gameInfo.gameDuration}`,
-					' getGame | match-svc ',
-				)
-
-				return gameInfo
-			})
-			.catch((err) => {
-				this.logger.error(
-					`Error while fetching game!\n\n${JSON.stringify(
-						err,
-						null,
-						4,
-					)}`,
-					' getGame | match-svc ',
-				)
-
-				return null
-			})
-	}
-
-	/**
-	 * @deprecated
-	 * This method uses the Riot Match API v4 to retrieve a list of matches for a specific Summoner
-	 *
-	 * @param accountId string `accountId` of a Summoner to use when retrieving matches
-	 * @param getLastX number Defaults to 10; number of matches to retrieve
-	 * @param includeGameData boolean Defaults to false; retrieves individual game data if true;
-	 *     otherwise, returns simple match data
-	 * @param queueType string Defaults to undefined; can be specified as a key from COMMON_QUEUE_TYPES
-	 *     to filter which matches to request
-	 * @returns A collection of either Match objects (default) or Game objects (includeGameData=true)
-	 */
-	async v4GetMatchlist(
-		accountId: string,
-		getLastX = 10,
-		includeGameData = false,
-		queueType: keyof typeof COMMON_QUEUE_TYPES = undefined,
-	): Promise<Match[] | Game[]> {
-		const apiKey = this.appService.getRiotToken()
-
-		// update value BEFORE hitting Riot API
-		if (getLastX < MIN_NUM_MATCHES) {
-			getLastX = MIN_NUM_MATCHES
-		}
-
-		if (getLastX > MAX_NUM_MATCHES) {
-			getLastX = MAX_NUM_MATCHES
-		}
-
-		const filterQueue =
-			queueType === undefined
-				? ''
-				: `&queue=${COMMON_QUEUE_TYPES[queueType].id}`
-
-		return firstValueFrom(
-			this.httpService.get(
-				`https://${REGION}.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?endIndex=${getLastX}${filterQueue}`,
-				{
-					headers: {
-						'Accept-Charset':
-							'application/x-www-form-urlencoded; charset=UTF-8',
-						'Accept-Language': 'en-US,en;q=0.9',
-						'X-Riot-Token': apiKey,
-					},
-				},
-			),
-		)
-			.then<Match[]>((resp) => {
-				const matchlist: Matchlist = resp.data
-
-				this.logger.log(
-					`${matchlist.totalGames} total matches, returning indices ${matchlist.startIndex} - ${matchlist.endIndex}`,
-					' getMatchlist | match-svc ',
-				)
-
-				return matchlist.matches
-			})
-			.then<Match[] | Game[]>((allMatches: Match[]) => {
-				const messageRetrieval = includeGameData
-					? `retrieving additional info for ${allMatches.length} indiviudal games...`
-					: 'returning info for matches w/o further data retrieval'
-
-				this.logger.log(
-					`includeGameData=${includeGameData}; ${messageRetrieval}`,
-					' getMatchlist | match-svc ',
-				)
-
-				return includeGameData
-					? Promise.all(
-							allMatches.map((match) =>
-								this.v4GetGame(match.gameId),
-							),
-							// could also be expressed (less efficiently) as below
-							// more info -
-							// https://www.freecodecamp.org/news/beware-of-chaining-array-methods-in-javascript-ef3983b60fbc
-							// allMatches.map((match) => match.gameId).map(this.v4GetGame),
-					  )
-					: Promise.resolve(allMatches)
 			})
 			.catch((err) => {
 				this.logger.error(
